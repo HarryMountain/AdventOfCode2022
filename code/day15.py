5630597efedd3b0ab6a4f8b32bbb8c272d6fa20c
Art Mountain <artmountain@btinternet.com>
2022-12-16 11:43:12 +0000

    Day 15

--- code/day15.py
+++ code/day15.py
@@ -1,6 +1,7 @@
+import copy
 test = False
-row_to_check = 10 if test else 2000000
 sensors = {}
+bounds = 20 if test else 4000000
 with open('../test_input_files/day15test.txt' if test else '../input_files/day15input.txt','r') as f:
     for line in f:   
         sense, beac = line.rstrip().split(':')
@@ -15,17 +16,36 @@ with open('../test_input_files/day15test.txt' if test else '../input_files/day15
         sensors[sensor_pos] = beacon_pos
 
 
-blocked_positions = set()
-for sensor, beacon in sensors.items():
-    dist = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
-    y_diff = abs(row_to_check - sensor[1])
-    x_dist = dist - y_diff
-    for x in range(sensor[0] - x_dist, sensor[0] + x_dist + 1):
-        blocked_positions.add(x)
+def get_range_on_row(row_to_check):
+    ranges = []
+    for sensor, beacon in sensors.items():
+        dist = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+        y_diff = abs(row_to_check - sensor[1])
+        x_dist = dist - y_diff
+        if x_dist >= 0:
+            ranges.append((sensor[0] - x_dist, sensor[0] + x_dist))
+    return sorted(ranges)
 
-for beacon in sensors.values():
-    if beacon[1] == row_to_check:
-        if beacon[0] in blocked_positions:
-            blocked_positions.remove(beacon[0])
 
-print(len(blocked_positions))
+def work_out_overlaps(ranges, start_index):
+    for i in range(start_index, len(ranges) - 1):
+        if ranges[i][1] >= ranges[i + 1][0] - 1:
+            new_start = ranges[i][0]
+            new_end = max(ranges[i + 1][1], ranges[i][1])
+            ranges[i] = (new_start, new_end)
+            ranges.pop(i + 1)
+            return work_out_overlaps(ranges, i)
+    return ranges
+
+
+for row in range(bounds):
+    if row % 10000 == 0:
+        print(row)
+    ranges = get_range_on_row(row)
+    merged = work_out_overlaps(ranges, 0)
+    if len(merged) > 1:
+        print(row, merged)
+
+x = 2829680 * 4000000
+y = 3411840
+print(x + y)
